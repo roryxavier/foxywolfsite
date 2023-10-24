9a4548d5020595bb269a27e59d374769e9fe0a04
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-10-24 23:34:25 +0800

    Update LinkList.ts

--- src/model/LinkList.ts
+++ src/model/LinkList.ts
@@ -1,7 +1,7 @@
 export default [
 	{ title: 'About Us', href: 'about' },
 	// { title: 'Timeline', href: 'timeline' },
-	{ title: 'Timer', href: 'timer' },
+	// { title: 'Timer', href: 'timer' },
 	{ title: 'Furry Convention', href: 'furcon' }
 	// { title: 'Travel Map', href: 'travel' },
 	// { title: 'Gallery', href: 'gallery' }

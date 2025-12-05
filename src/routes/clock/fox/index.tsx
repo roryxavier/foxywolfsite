85b7573b7607bd1ec15171da0e768390c3e5e752
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2025-12-05 23:56:35 +0800

    make counter center

--- src/routes/clock/fox/index.tsx
+++ src/routes/clock/fox/index.tsx
@@ -16,67 +16,82 @@ function RouteComponent() {
 					className="
             w-full
             flex
-            flex-row
+            flex-column
             flex-auto
             pt-8
             min-h-screen
         "
 				>
-					<div className={"flex-3"} />
 					<div className={"flex-5 text-white"}>
-						<div className="flex flex-col flex-auto gap-4">
+						<div
+							className="flex flex-col flex-auto gap-4"
+							style={{ minHeight: "calc(100dvh - 8rem)" }}
+						>
 							<h1 className={"text-4xl font-bold text-center"}>
 								My Important Foxes Clock
 							</h1>
 
-							<div>
-								<TextEffect
-									text={"Time I meet with Yellow Fox"}
-									delay={50}
-									className={"text-2xl font-bold text-yellow-200 text-center"}
-								/>
+							<div
+								style={{
+									height: "100%",
+									display: "flex",
+									flexGrow: "1",
+									flexDirection: "column",
+									alignItems: "center",
+									justifyContent: "center",
+								}}
+							>
+								<div>
+									<TextEffect
+										text={"Time I meet with Yellow Fox"}
+										delay={50}
+										className={"text-2xl font-bold text-yellow-200 text-center"}
+									/>
 
-								<TextEffect
-									text={"2021-03-05 01:19"}
-									delay={100}
-									className={"text-sm text-black text-center"}
-								/>
+									<TextEffect
+										text={"2021-03-05 01:19"}
+										delay={100}
+										className={"text-sm text-black text-center"}
+									/>
+								</div>
+								<div>
+									<Countup
+										year={2021}
+										month={3}
+										day={5}
+										minute={1}
+										hour={19}
+									></Countup>
+								</div>
+
+								<hr className="border-t-2 border-gray-700 opacity-40" />
+
+								<div>
+									<TextEffect
+										text={"Time I meet with Orange Fox"}
+										delay={50}
+										className={"text-2xl font-bold text-[#FF8C00] text-center"}
+									/>
+									<TextEffect
+										text={"2025-03-02 10:20"}
+										delay={100}
+										className={"text-sm text-black text-center"}
+									/>
+								</div>
+								<div>
+									<Countup
+										year={2025}
+										month={3}
+										day={2}
+										hour={10}
+										minute={20}
+									></Countup>
+								</div>
+
+								<hr className="border-t-2 border-gray-700 opacity-40" />
 							</div>
-							<div>
-								<Countup
-									year={2021}
-									month={3}
-									day={5}
-									minute={1}
-									hour={19}
-								></Countup>
-							</div>
-							<hr className="border-t-2 border-gray-700 opacity-40" />
-							<div>
-								<TextEffect
-									text={"Time I meet with Orange Fox"}
-									delay={50}
-									className={"text-2xl font-bold text-[#FF8C00] text-center"}
-								/>
-								<TextEffect
-									text={"2025-03-02 10:20"}
-									delay={100}
-									className={"text-sm text-black text-center"}
-								/>
-							</div>
-							<div>
-								<Countup
-									year={2025}
-									month={3}
-									day={2}
-									hour={10}
-									minute={20}
-								></Countup>
-							</div>
-							<hr className="border-t-2 border-gray-700 opacity-40" />
 						</div>
 					</div>
-					<div className={"flex-3"} />
 				</div>
 			</AnimatedBg>
 		</>

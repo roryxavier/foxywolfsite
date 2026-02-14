d4fef5459c5f1d7d72947ab0ee0cf9f6066375ed
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2026-02-14 20:22:10 +0800

    extract routes as pages

---
+++ src/pages/about/about.page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import Section from './components/Section.svelte';
+	import SectionTitle from './components/SectionTitle.svelte';
+	import Row from './components/Row.svelte';
+
+	import TOGETHER_PNG from '@/assets/together-w520h416.png';
+</script>
+
+<div>
+	<div class="flex flex-col items-center justify-start" style="min-height: 100dvh">
+		<div
+			class="flex flex-col items-center justify-start p-6 gap-8"
+			style="max-width: 35rem; padding-bottom: 20rem;"
+		>
+			<img class="w-full max-w-xs rounded-xl" src={TOGETHER_PNG} alt="Together" />
+
+			<p class="max-w-xl">
+				Hello, I'm Rory, a blue wolf, and right next to me is Jimmy, a yellow fox. We have been
+				dating since April 2, 2021. We are both coders and have collaborated to build this website
+				together. The purpose of this website is to serve as a dedicated memory lane for us,
+				capturing our travels and experiences at furry conventions.
+			</p>
+
+			<p class="max-w-xl">
+				Please feel free to explore the various pages we have created. You will find a timeline
+				highlighting the significant milestones of our relationship, a gallery showcasing our
+				artwork, and even a travel map documenting the places we have visited.
+			</p>
+
+			<p class="max-w-xl">
+				Thank you for taking the time to visit our website. We hope you enjoy browsing through our
+				memories. Have a fantastic day!
+			</p>
+
+			<Section>
+				<SectionTitle>Reference Link</SectionTitle>
+				<Row title="Rory Xavier" menus={[{ title: 'Visit', href: 'https://ref.rorywolf.net/' }]} />
+			</Section>
+
+			<Section>
+				<SectionTitle>Social Link</SectionTitle>
+				<Row
+					title="Rory Xavier"
+					menus={[
+						{ title: 'Twitter', href: 'https://twitter.com/rory_xavi' },
+						{ title: 'Telegram', href: 'https://t.me/RoryXavier' }
+					]}
+				/>
+				<Row
+					title="Jimmy Fox"
+					menus={[
+						{ title: 'Twitter', href: 'https://twitter.com/jimmykajun' },
+						{ title: 'Telegram', href: 'https://t.me/Jimmy612' }
+					]}
+				/>
+			</Section>
+
+			<Section>
+				<SectionTitle>Websites</SectionTitle>
+				<Row title="Rory Xavier" menus={[{ title: 'Visit', href: 'https://rorywolf.net/' }]} />
+				<Row title="Jimmy Fox" menus={[{ title: 'Visit', href: 'http://jimmychane.com/' }]} />
+			</Section>
+		</div>
+	</div>
+</div>
